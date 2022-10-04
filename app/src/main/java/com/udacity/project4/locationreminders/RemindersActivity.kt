31a377bd5ca23b479a2a6a4d7c751416bff1c783
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-10-04 16:13:28 +0200

    Update dependencies

---
+++ app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -0,0 +1,33 @@
+package com.udacity.project4.locationreminders
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.udacity.project4.R
+
+/**
+ * The RemindersActivity that holds the reminders fragments
+ */
+class RemindersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reminders)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // Find NavHostFragment and NavController
+                val navHostFragment =
+                    supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+                (navHostFragment as NavHostFragment).navController.popBackStack()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
