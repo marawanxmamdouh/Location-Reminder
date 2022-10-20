044e2d8bd613025548ba1292adbd2bfc7020ce7d
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-10-20 14:26:37 +0200

    Testing to the RemindersListViewModel

--- app/src/test/java/com/udacity/project4/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,94 @@
 package com.udacity.project4.reminderslist
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import com.udacity.project4.MainCoroutineRule
+import com.udacity.project4.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.runner.RunWith
+import kotlinx.coroutines.test.pauseDispatcher
+import kotlinx.coroutines.test.resumeDispatcher
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.After
+import org.junit.Assert.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.koin.core.context.stopKoin
 
-@RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var fakeDataSource: FakeDataSource
+    private lateinit var reminderListViewModel: RemindersListViewModel
+
+    private val remindersList = listOf(
+        ReminderDTO(
+            "Random title",
+            "Random description",
+            "Random location",
+            10.44206,
+            81.58948
+        ),
+        ReminderDTO(
+            "Random title",
+            "Random description",
+            "Random location",
+            44.78961,
+            -138.08021
+        ),
+        ReminderDTO(
+            "Random title",
+            "Random description",
+            "Random location",
+            -9.49210,
+            -124.72571
+        )
+    )
+
+    private val reminder1 = remindersList[0]
+    private val reminder2 = remindersList[1]
+    private val reminder3 = remindersList[2]
+
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
+
+    @Test
+    fun getRemindersList() {
+        val remindersList = mutableListOf(reminder1, reminder2, reminder3)
+        fakeDataSource = FakeDataSource(remindersList)
+        reminderListViewModel =
+            RemindersListViewModel(ApplicationProvider.getApplicationContext(), fakeDataSource)
+        assertThat(reminderListViewModel.remindersList.value?.size, `is`(remindersList.size))
+    }
+
+    @Test
+    fun check_loading() {
+        fakeDataSource = FakeDataSource(remindersList.toMutableList())
+        reminderListViewModel =
+            RemindersListViewModel(ApplicationProvider.getApplicationContext(), fakeDataSource)
+        mainCoroutineRule.pauseDispatcher()
+        reminderListViewModel.loadReminders()
+        assertThat(reminderListViewModel.showLoading.value, `is`(true))
+        mainCoroutineRule.resumeDispatcher()
+        assertThat(reminderListViewModel.showLoading.value, `is`(false))
+    }
+
+    @Test
+    fun returnError() {
+        fakeDataSource = FakeDataSource(remindersList.toMutableList())
+        reminderListViewModel =
+            RemindersListViewModel(ApplicationProvider.getApplicationContext(), fakeDataSource)
+        reminderListViewModel.loadReminders()
+        assertThat(reminderListViewModel.showSnackBar.value, `is`("Test exception"))
+    }
 
 }
