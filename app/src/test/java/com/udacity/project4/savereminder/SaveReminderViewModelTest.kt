508bddb1fa24554d17bf65204f874f5b22f19133
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-10-20 15:11:00 +0200

    Testing to the SaveReminderView

--- app/src/test/java/com/udacity/project4/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,95 @@
 package com.udacity.project4.savereminder
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import com.udacity.project4.MainCoroutineRule
+import com.udacity.project4.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.pauseDispatcher
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.After
+import org.junit.Assert.assertThat
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
 
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
-    //TODO: provide testing to the SaveReminderView and its live data objects
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
+    private lateinit var fakeDataSource: FakeDataSource
+    private lateinit var reminderListViewModel: RemindersListViewModel
+
+    private val remindersList = listOf(
+        ReminderDTO(
+            "Random title",
+            "Random description",
+            "Random location",
+            10.44206,
+            81.58948
+        ),
+        ReminderDTO(
+            "Random title",
+            "Random description",
+            "Random location",
+            44.78961,
+            -138.08021
+        ),
+        ReminderDTO(
+            "Random title",
+            "Random description",
+            "Random location",
+            -9.49210,
+            -124.72571
+        )
+    )
+
+    private val reminder1 = remindersList[0]
+    private val reminder2 = remindersList[1]
+    private val reminder3 = remindersList[2]
+
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
+
+    @Test
+    fun getRemindersList() {
+        val remindersList = mutableListOf(reminder1, reminder2, reminder3)
+        fakeDataSource = FakeDataSource(remindersList)
+        reminderListViewModel =
+            RemindersListViewModel(ApplicationProvider.getApplicationContext(), fakeDataSource)
+        reminderListViewModel.loadReminders()
+        assertThat(reminderListViewModel.remindersList.value?.size, `is`(3))
+    }
+
+    @Test
+    fun check_loading() {
+        fakeDataSource = FakeDataSource(mutableListOf())
+        reminderListViewModel =
+            RemindersListViewModel(ApplicationProvider.getApplicationContext(), fakeDataSource)
+        mainCoroutineRule.pauseDispatcher()
+        reminderListViewModel.loadReminders()
+        assertThat(reminderListViewModel.showLoading, `is`(true))
+    }
+
+    @Test
+    fun returnError() {
+        fakeDataSource = FakeDataSource(null)
+        reminderListViewModel =
+            RemindersListViewModel(ApplicationProvider.getApplicationContext(), fakeDataSource)
+        reminderListViewModel.loadReminders()
+        assertThat(reminderListViewModel.showSnackBar, `is`("No reminders found"))
+    }
 
 }
