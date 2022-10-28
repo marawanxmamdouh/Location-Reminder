cfdb1a55173b37bb18e32cadc574fa61462bd1ff
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-10-28 22:59:47 +0200

    Handle multiple reminders at the same location.

--- app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
@@ -2,15 +2,12 @@ package com.udacity.project4.locationreminders.geofence
 
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import androidx.core.app.JobIntentService
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingEvent
-import com.udacity.project4.R
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
-import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.utils.sendNotification
 import kotlinx.coroutines.*
@@ -18,6 +15,7 @@ import org.koin.android.ext.android.inject
 import kotlin.coroutines.CoroutineContext
 
 private const val TAG = "GeofenceTransitionsJobIntentService"
+
 class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
 
     private var coroutineJob: Job = Job()
@@ -46,18 +44,8 @@ class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
     // : get the request id of the current geofence
     private fun sendNotification(triggeringGeofences: List<Geofence>) {
         var requestId = ""
-
-        requestId = when {
-            triggeringGeofences.isNotEmpty() ->
-            {
-                Log.i(TAG, "sendNotification (line 63): ${triggeringGeofences[0].requestId}")
-                triggeringGeofences[0].requestId
-            }
-
-            else -> {
-                Log.i(TAG, "sendNotification (line 59): No Geofence Trigger Found! Abort mission!")
-                return
-            }
+        triggeringGeofences.forEach {
+            requestId = it.requestId
         }
 
         //Get the local repository instance
